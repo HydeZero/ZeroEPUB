d51671c9c5e90ea6f9e5df5319eb59d4362dfe5f
Zachary Smith <128327411+HydeZero@users.noreply.github.com>
2024-07-11 14:09:40 -0500

    Make the exit button work but not exit the app

--- ZeroEPUB/Views/MainWindow.axaml.cs
+++ ZeroEPUB/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 
 namespace ZeroEPUB.Views;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ExitApp(object source, RoutedEventArgs args)
+    {
+        Debug.WriteLine("It would exit here");
     }
 }
